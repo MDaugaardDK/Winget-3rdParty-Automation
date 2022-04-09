c9e08ed47d93ef7f11c32cb14289d390a53425d7
MDaugaardDK <48121840+MDaugaardDK@users.noreply.github.com>
2022-04-09 20:04:16 +0200

    Add files via upload

---
+++ Java Discovery Script.ps1
@@ -0,0 +1,28 @@
+﻿$AppName = "Java*"
+$CurrentVersion = "8.0.3210.7"
+$FindApp = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall -Recurse -ErrorAction SilentlyContinue | Foreach {Get-ItemProperty $_.pspath} -ErrorAction SilentlyContinue | Where-Object {$_.DisplayName -like $AppName} -ErrorAction SilentlyContinue
+
+If ($FindApp.DisplayName -eq $null){
+Write-Output $false
+}
+else{
+	If ($FindApp.DisplayVersion -le $CurrentVersion)
+        {
+
+		    If ($FindApp.DisplayName -like $AppName -and $FindApp.DisplayVersion -lt $CurrentVersion)
+                {
+Write-Host "Old version of Java 8 Update were detected"
+			        Write-Output $false
+		        }
+		    Else
+                {
+			        Write-Host "Current version of Java 8 Update were detected, but older versions were not found. Do nothing."
+			        Write-Output $true
+		        }
+	    }
+	Else
+        {
+		    Write-Host "Current version of Java 8 Update were not detected. Do nothing."
+		    Write-Output $true
+	    }
+}
---
+++ Java Remediation Script.ps1
@@ -0,0 +1,35 @@
+﻿$AppName = "Java*"
+$CurrentVersion = "8.0.3210.7"
+$PreviousVersion = "8.0.3010.9"
+$InstallString = "\\ConfigurationManager$\SoftwareContent\AppInstalllation"
+$FindApp = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall -Recurse -ErrorAction SilentlyContinue | Foreach {Get-ItemProperty $_.pspath} -ErrorAction SilentlyContinue | Where-Object {$_.DisplayName -like $AppName} -ErrorAction SilentlyContinue
+Add-AppxPackage “$InstallString\WinGet.appxbundle”
+
+If ($FindApp.DisplayName -eq $null)
+    {
+    winget install -e --id Oracle.JavaRuntimeEnvironment --silent --accept-source-agreements
+    }
+else
+    {
+	If ($FindApp.DisplayVersion -le $CurrentVersion){
+
+		If ($FindApp.DisplayName -like $AppName -and $FindApp.DisplayVersion -lt $CurrentVersion){
+			Write-Host "Older versions of Java 8 Update were detected. Trying to Update."
+
+				Try{
+					Get-Package -Name $AppName -MaximumVersion $PreviousVersion -ProviderName "msi" | Uninstall-Package -Force
+                    winget install -e --id Oracle.JavaRuntimeEnvironment --silent --accept-source-agreements
+				}
+				Catch{
+					Write-Host "Failed to uninstall older versions of Java 8 Update"
+					Write-Host "$_.Exception.Message"
+				}
+		}
+		Else{
+			Write-Host "Current version of Java 8 Update were detected, but older versions were not found. Do nothing."
+		}
+	}
+	Else{
+		Write-Host "Older version of Java 8 Update were not detected. Do nothing."
+	}
+}
